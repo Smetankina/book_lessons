5f77978900e5c9e9a07cf1d6298d41db33c9a502
Anna <an.kunitskaya@gmail.com>
2021-10-26 09:44:33 +0300

    Bot

--- Bot.py
+++ Bot.py
@@ -1,19 +1,45 @@
-def get_intent(question):
-    return 'hello'
+import random
+
+BOT_CONFIG = {
+    'intents':{
+        "hello":{
+            'examples': ['Привет', 'Добрый день', 'Шалом', 'здравствуйте'],
+            'responses': ['Привет, человек', 'Доброго времени суток']
+        },
+    'bye':{
+        'examples': ['Пока', 'Досвидания', 'Прощайте', 'До свидания'],
+        'responses': ['Счастиливо', 'Если что я тут', 'Еще увидимся']
+    }
+    },
+    'failure_phrases': [
+        'Попробуйте написать по-другому'
+        'Что-то непонятно'
+        'Я же всего лишь бот'
+    ]}
+
+intent = input()
+
+def get_intent(intent):
+    for intent in BOT_CONFIG['intents']:
+        print[intent]
 
 
 def get_answer_by_intent(intent):
-    if answer == 'hello':
-        return "Привет"
+    if intent in BOT_CONFIG['intents']:
+        phrases = BOT_CONFIG['intents'][intent]['responses']
+        return random.choice(phrases)
 
 def generate_answer_by_text(question):
     return #TODO 3th day
 
 
 def get_failure_phrase():
-    return 'Я не поняла'
+    phrases = BOT_CONFIG['failure_phrases']
+    return random.choice(phrases)
+
 
 def bot(question):
+
     #NLU
     intent = get_intent(question)
     #Получение ответа
@@ -31,10 +57,13 @@ def bot(question):
     return answer
 
 
-question = None
 
-#while question in ['exit', 'выход']:
-while question != 'exit':
-    question = input()
+
+"""
+question = None
+     while question != 'exit':
+        question = input()   
     answer = question
-    print(answer)
+    print(answer)"""
+
+bot(question='hello')
